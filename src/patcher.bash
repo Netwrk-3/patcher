6a09d8f4cc58bc2f63ea215ffc731942cfdcc641
vm-hacker <104187311+vm-hacker@users.noreply.github.com>
2022-04-25 09:13:35 +0530

    Update patcher.bash

--- src/patcher.bash
+++ src/patcher.bash
@@ -3,14 +3,13 @@
 
 opt=$1
 kernel=$2
-version="0.1.13-beta"
+iversion="0.1.14-beta"
 
 help_menu () {
    echo 'patcher -s [scan a kernel version for vulnebilities]'
    echo 'patcher -v [print the version of patcher]'
    echo 'patcher -h [print the help menu]'
    echo 'patcher -c [cleanup your system and free up disk space]'
-   echo 'patcher -ps [show the command to patch a certain vulnebility]'
    echo 'patcher kernel [display the kernel you are currently using]'
    echo "patcher devs [patcher development team list]"
 }
@@ -44,16 +43,6 @@ elif [ "$opt" = "kernel" ];then
         host_arch=$(uname -m) # Get the CPU's rchitecure
         echo "Endpoint kernel: $host_kernel"
         echo "Endpoint architecture: $host_arch"
-elif [ "$opt" = "-ps" ];then
-    echo "Enter the vuln's CVE id: "
-    read vuln
-    if [ $vuln = "CVE-2022-0847" ]
-    then
-        echo 'Run the following command as root:'
-        echo 'sysctl kernel.unprivileged_bpf_disabled=1 && sysctl kernel.unprivileged_clone=0 && sysctl lvm.unprivileged_userfualtfd=0 && sysctl -p'
-    else
-        echo 'Error please enter a valid CVE id'
-    fi
 elif [ "$opt" = "devs" ];then
     echo 'Patcher development team:'
     echo '1. Venkatesh Mishra (head developer)'
