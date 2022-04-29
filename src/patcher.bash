1e2ebba4275ee990afda8b24a889a2fdd00eaa2d
vm-hacker <104187311+vm-hacker@users.noreply.github.com>
2022-04-29 10:05:26 +0530

    Update patcher.bash

--- src/patcher.bash
+++ src/patcher.bash
@@ -3,7 +3,9 @@
 
 opt=$1
 kernel=$2
-version="0.1.18-stable"
+version="0.1.21-beta"
+RED='\033[0;31m'
+NC='\033[0m'
 
 help_menu () {
    echo 'patcher -s or patcher --scan [scan a kernel version for vulnebilities]'
@@ -46,6 +48,12 @@ if [ "$opt" = "-s" ] || [ "$opt" = "--scan" ];then
         elif [ $kernel = "4.8.13" ];then
                 echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
                 echo "CVE-2016-10150"
+        elif [ $kernel = "2.6.34" ];then
+                echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
+                echo "CVE-2010-252"
+        elif [ $kernel = "4.3" ] || [ $kernel = "4.2" ];then
+                echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
+                echo "CVE-2017-13715"
         else    
                 echo "patcher-db did not find any vulnebilities for Linux kernel version $kernel"
         fi
@@ -81,12 +89,15 @@ elif [ "$opt" = "-c" ] || [ "$opt" = "--clean" ];then
     # on rhel / fedora based systems
     # echo 'removing orphan packages
     # dnf clean && dnf autoremove
+    # on arch linux / manjaro based systems:
+    # echo 'cleaning package cache and removing synced repositories:'
+    # pacman -Scc && rm /var/lib/pacman/sync/*db
 elif [ "$opt" = "update" ];then
      echo "updating patcher..."
      git clone https://github.com/Emph-Inc/patcher.git && cd patcher && cd src && cp ./patcher.bash ./patcher && chmod +x ./patcher && cp ./patcher /usr/local/bin/ && cd .. && cd .. && rm -rf patcher/
 elif [ "$opt" = "-hr" ] || [ "$opt" = "--harden" ];then
-   echo "Hardening you linux kernel..."
+   echo "Hardening your linux kernel..."
    systctl kernel.pid_max = 65536; sysctl kernel.core_uses_pid = 1;sysctl kernel.ctrl-alt-del = 0;sysctl kernel.shmmax = 268435456;sysctl kernel.shmall = 268435456;sysctl kernel.printk=3 3 3 3;sysctl kernel.sysrq=4; sysctl kernel.kptr_restrict=2; sysctl kernel.unprivileged_bpf_disabled=1;sysctl kernel.kexec_load_disabled=1;sysctl kernel.unprivileged_userns_clone=0; sysctl kernel.perf_event_paranoid=3;sysctl  kernel.yama.ptrace_scope=2;sysctl kernel.core_uses_pid = 1 && sysctl -p
 else
-        echo "Error please enter a valid argument (use patcher -h to see available arguments)"
+   printf "${RED}Error:${NC} please enter a valid argument (use patcher -h to see valid arguments)\n"
 fi
