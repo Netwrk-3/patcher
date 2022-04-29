dd2ec95ea7ace844f4ad3745a31b137c40140c67
vm-hacker <104187311+vm-hacker@users.noreply.github.com>
2022-04-29 09:06:02 +0530

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,2 +1,15 @@
 #!/bin/bash
-cd src && chmod +x patcher && cp ./patcher /usr/local/bin
+
+check_priv()
+{
+  if [ "$(id -u)" -ne 0 ]; then
+    err "you must be root"
+  fi
+}
+
+main() {
+	check_priv
+	cd src && chmod +x patcher && cp ./patcher /usr/local/bin
+}
+
+main
