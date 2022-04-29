0a787331e998c781187ddbf19685b21460d5dce3
vm-hacker <104187311+vm-hacker@users.noreply.github.com>
2022-04-29 10:17:56 +0530

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check_priv()
+{
+  if [ "$(id -u)" -ne 0 ]; then
+    err "you must be root"
+  fi
+}
+
+main() {
+	check_priv
+  echo "installing patcher..."
+	cd src && chmod +x patcher && cp ./patcher /usr/local/bin
+}
+
+main
