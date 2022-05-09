f5146ae0f5c220d2e00c4fb764dde44d63a25e69
vm.opensource <104187311+vm-hacker@users.noreply.github.com>
2022-05-09 12:01:43 +0530

    Update patcher.bash

--- src/patcher.bash
+++ src/patcher.bash
@@ -1,24 +1,23 @@
 # Copyright (c) 2022 Venkatesh Mishra. All rights reserved
 # Copyright (C) 2022 Netwrk-3, Inc. All rights reserved.
 #!/bin/bash
-
 opt=$1
 kernel=$2
-version="0.1.24-stable"
+version="0.1.28-stable"
 RED='\033[0;31m'
 NC='\033[0m'
-
 help_menu () {
    echo 'patcher -s or patcher --scan [scan a kernel version for vulnebilities]'
    echo 'patcher -v or patcher --version [print the version of patcher]'
    echo 'patcher -h or patcher --help [print the help menu]'
    echo 'patcher -c or patcher --clean [cleanup your system and free up disk space]'
    echo 'patcher kernel [display the kernel you are currently using]'
+   echo "patcher ip [display your system's public ip adress]"
+   echo "patcher --restart [reboots your machine securely using patcher's reboot scirpt]"
    echo "patcher -hr or patcher --harden [harden the endpoint linux kernel]"
    echo "patcher update [update patcher to the latest stable release]"
    echo "patcher devs [patcher development team list]"
 }
-
 update_patcher() {
         if [ "$(id -u)" -ne 0 ]; then
                 printf "${RED}[!] Error:${NC} Patcher needs to be updated with root privilages\n"
@@ -27,10 +26,15 @@ update_patcher() {
                 git clone https://github.com/Emph-Inc/patcher.git && cd patcher && cd src && cp ./patcher.bash ./patcher && chmod +x ./patcher && cp ./patcher /usr/local/bin/ && cd .. && cd .. && rm -rf patcher/
         fi
 }
-
-# patcher's Linux kernel vulnebility scanner
+secure_reboot() {
+	if [ "$(id -u)" -ne 0 ];then
+		printf "${RED}[!] Error:${NC} Try again with sudo or doas!\n"
+	else
+		killall $USER && echo 3 > /proc/sys/vm/drop_caches && reboot # works best with systemd
+	fi
+}
 if [ "$opt" = "-s" ] || [ "$opt" = "--scan" ];then
-        if [ $kernel = "4.8.3" ];then 
+        if [ $kernel = "4.8.3" ];then
                 echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
                 echo "CVE-2016-5195"
         elif [ $kernel = "5.14.01" ] || [ $kernel = "5.8" ] || [ $kernel = "5.9" ] || [ $kernel = "5.10" ] || [ $kernel = "5.12" ];then
@@ -51,7 +55,7 @@ if [ "$opt" = "-s" ] || [ "$opt" = "--scan" ];then
                 echo "CVE-2015-8812"
         elif [ $kernel = "3.2" ] || [ $kernel = "3.13" ];then
                 echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
-                echo "CVE-2016-10229" 
+                echo "CVE-2016-10229"
         elif [ $kernel = "3.13.6" ];then
                 echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
                 echo "CVE-2014-2523"
@@ -67,22 +71,27 @@ if [ "$opt" = "-s" ] || [ "$opt" = "--scan" ];then
         elif [ $kernel = "4.5.2" ];then
                 echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
                 echo "CVE-2016-7117"
-        else    
+        elif [ $kernel = "2.6.8" ];then
+                echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
+                echo "CVE-2009-0065"
+        elif [ $kernel = "4.4" ];then
+                echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
+                echo "CVE-2015-8787"
+        elif [ $kernel = "5.12.13" ];then
+                echo "Kernel verison $kernel is vulnerable to the following vulnerablilities: "
+                echo "CVE-2021-46283"
+        else
                 echo "patcher-db did not find any vulnebilities for Linux kernel version $kernel"
         fi
 
 elif [ "$opt" = "-v" ] || [ "$opt" = "--version" ];then
         echo "Patcher version $version"
-
 elif [ "$opt" = "-h" ] || [ "$opt" = "--help" ];then
         help_menu
-# Print the endpoint kernel
 elif [ "$opt" = "kernel" ];then
         host_kernel=$(uname -rs) # Get the name & version of the kernel
         host_arch=$(uname -m) # Get the CPU's rchitecure
-        echo "Endpoint kernel: $host_kernel"
-        echo "Endpoint architecture: $host_arch"
-
+        echo "Endpoint kernel: $host_kernel" && echo "Endpoint architecture: $host_arch"
 elif [ "$opt" = "devs" ];then
     echo 'Patcher development team:'
     echo '1. Venkatesh Mishra (head developer)'
@@ -96,24 +105,23 @@ elif [ "$opt" = "-c" ] || [ "$opt" = "--clean" ];then
             echo 'deleting cache files...'
             rm -rf /home/$USER/.cache/*
             echo 'deleting temperary files...'
-            rm -rf /tmp/*
+            rm -rf /tmp/* && rm -rf /var/tmp/*
             echo 'dropping cached memory...'
             echo 3 > /proc/sys/vm/drop_caches
-            # on debian / ubuntu based systems
-            # echo 'removing orphan packages
-            # apt clean && apt autoremove
-            # on rhel / fedora based systems
-            # echo 'removing orphan packages
-            # dnf clean && dnf autoremove
-            # on arch linux / manjaro based systems:
-            # echo 'cleaning package cache and removing synced repositories:'
-            # pacman -Scc && rm /var/lib/pacman/sync/*db
         fi
 elif [ "$opt" = "update" ];then
      update_patcher
+elif [ "$opt" = "ip" ];then
+	ip=$(curl -s 'api.ipify.org') && echo "Public IP adress: $ip"
+elif [ "$opt" = "--restart" ];then
+	secure_reboot
 elif [ "$opt" = "-hr" ] || [ "$opt" = "--harden" ];then
-   echo "Hardening your linux kernel..."
-   systctl kernel.pid_max = 65536; sysctl kernel.core_uses_pid = 1;sysctl kernel.ctrl-alt-del = 0;sysctl kernel.shmmax = 268435456;sysctl kernel.shmall = 268435456;sysctl kernel.printk=3 3 3 3;sysctl kernel.sysrq=4; sysctl kernel.kptr_restrict=2; sysctl kernel.unprivileged_bpf_disabled=1;sysctl kernel.kexec_load_disabled=1;sysctl kernel.unprivileged_userns_clone=0; sysctl kernel.perf_event_paranoid=3;sysctl  kernel.yama.ptrace_scope=2;sysctl kernel.core_uses_pid = 1 && sysctl -p
+   if [ "$(id -u)" -ne 0 ]; then
+        printf "${RED}[!] Error:${NC} Harden your system using sudo or doas, please!\n"
+   else
+   	echo "Hardening your linux kernel..."
+   	systctl kernel.pid_max = 65536; sysctl kernel.core_uses_pid = 1;sysctl kernel.ctrl-alt-del = 0;sysctl kernel.shmmax = 268435456;sysctl kernel.shmall = 268435456;sysctl kernel.printk=3 3 3 3;sysctl kernel.sysrq=4; sysctl kernel.kptr_restrict=2; sysctl kernel.unprivileged_bpf_disabled=1;sysctl kernel.kexec_load_disabled=1;sysctl kernel.unprivileged_userns_clone=0; sysctl kernel.perf_event_paranoid=3;sysctl  kernel.yama.ptrace_scope=2;sysctl kernel.core_uses_pid = 1 && sysctl -p
+   fi
 else
    printf "${RED}[!] Error:${NC} please enter a valid argument (use patcher -h to see valid arguments)\n"
 fi
