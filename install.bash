df6aeb0e408c1cede546863169382fbe9cc3df3b
vm.opensource <104187311+vm-hacker@users.noreply.github.com>
2022-08-30 12:11:59 +0530

    Update install.bash

--- install.bash
+++ install.bash
@@ -5,12 +5,13 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 main() {
-	if [ "$(id -u)" -ne 0 ]; then
-   		printf "${RED}[!] Error:${NC} patcher's installation script requires evelavted privilages!\n"
-  	else	
-		echo "installing patcher..."
-		cd src && chmod +x patcher && cp ./patcher /usr/local/bin
-	fi
+        if [ "$(id -u)" -ne 0 ]; then
+                printf "${RED}[!] Error:${NC} patcher's installation script requires evelavted privilages!\n"
+        else
+                echo "installing patcher..."
+                cd src && cp patcher.bash patcher &&chmod +x patcher && cp ./patcher /usr/local/bin
+                echo "Done!"
+        fi
 }
 
 main
